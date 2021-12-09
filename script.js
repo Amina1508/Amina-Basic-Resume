5d2fb69fe1507db23ccc6a81454bf784c7c4d284
Amina1508 <93791771+Amina1508@users.noreply.github.com>
2021-12-09 23:24:57 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+document.write('Welcome to my CV!');
+
+mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; 
+  document.documentElement.scrollTop = 0; 
+}
+
+const colours = ["pink", "white", "purple"];
+
+console.log(schedule[0]) 
+
+var year = 4 ; // i hope it will stay 4!
+
+year = year + 1;  // 5 godina
+
+var coursename = 'CS 412' // this is the course i am going to
+
+
+console.log (year);
+
+var year = true;  // Boolean
+
